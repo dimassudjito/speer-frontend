660968fcea541e8ae62696f624092621d204d2a9
dimas sudjito <dimassudjito@localhost.localdomain>
2021-03-07 11:10:07 -0700

    burger menu now can open

--- src/components/landing/Burger.js
+++ src/components/landing/Burger.js
@@ -1,19 +1,33 @@
 import React, {useState} from 'react';
 import {GiHamburgerMenu} from 'react-icons/gi';
+import '../../styles/landing.css';
 
 const Pricing = () => {
+    const [open, setOpen] = useState(false);
+
+    const clickBurger = () => {
+        setOpen(!open);
+    }
 
     return (
-        <div>
-            <div className="burger">
-            <GiHamburgerMenu size={30}/>
-            <h2 className="logo">EXP|CON</h2>
-            </div>
-            <nav class="nav flex-column">
-                <a class="nav-link active" href="#">Active</a>
-                <a class="nav-link" href="#">Link</a>
-                <a class="nav-link" href="#">Link</a>
-            </nav>
+        <div>  
+            {!open && (
+                <div className="burger">
+                <GiHamburgerMenu size={30} onClick={clickBurger}/>
+                <h2 className="logo">EXP|CON</h2>
+                </div>
+            )}  
+            {open && (
+                <div className="burger-black">
+                <GiHamburgerMenu size={30} onClick={clickBurger}/>
+                <h2 className="logo">EXP|CON</h2>
+                <nav class="nav flex-column">
+                    <a class="nav-link burger-item">WHAT IS IT</a>
+                    <a class="nav-link burger-item">PERKS</a>
+                    <a class="nav-link burger-item">PRICING</a>
+                </nav>
+                </div>
+            )}
         </div>
   )
 }
