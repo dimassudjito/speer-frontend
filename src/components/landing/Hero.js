36e2ccd55285fd78e96f92915f728b517281b615
dimas sudjito <dimassudjito@localhost.localdomain>
2021-03-06 18:20:43 -0700

    start red section

--- src/components/landing/Hero.js
+++ src/components/landing/Hero.js
@@ -5,26 +5,26 @@ const Hero = () => {
 
     return (
         <div className="hero">
-            <div class="overlay">
+            <div className="overlay">
                 <h1>INTERACTIVE CONCERT EXPERIENCE</h1>
                 <p>Experience your favourite artists like never before and from the comfort of your own home</p>
                 <button className="btn btn-light">TRY IT NOW</button>
             </div>
-            <div id="heroCarousel" class="carousel slide" data-bs-ride="carousel">
-                <div class="carousel-indicators">
-                    <button type="button" data-bs-target="#heroCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <div id="heroCarousel" className="carousel slide" data-bs-ride="carousel">
+                <div className="carousel-indicators">
+                    <button type="button" data-bs-target="#heroCarousel" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
                     <button type="button" data-bs-target="#heroCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
                     <button type="button" data-bs-target="#heroCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
                 </div>
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img src="./hero1.jpg" class="d-block w-100" style={{maxHeight:"500px"}} alt="concert image"/>
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                        <img src="./hero1.jpg" className="d-block w-100" style={{maxHeight:"500px"}} alt="concert image"/>
                     </div>
-                    <div class="carousel-item">
-                        <img src="./hero2.jpg" class="d-block w-100" style={{maxHeight:"500px"}} alt="concert image"/>
+                    <div className="carousel-item">
+                        <img src="./hero2.jpg" className="d-block w-100" style={{maxHeight:"500px"}} alt="concert image"/>
                     </div>
-                    <div class="carousel-item">
-                        <img src="./hero3.jpg" class="d-block w-100" style={{maxHeight:"500px"}} alt="concert image"/>
+                    <div className="carousel-item">
+                        <img src="./hero3.jpg" className="d-block w-100" style={{maxHeight:"500px"}} alt="concert image"/>
                     </div>
                 </div>
         </div>
--- src/components/landing/Red.js
+++ src/components/landing/Red.js
@@ -5,10 +5,19 @@ const Red = () => {
 
     return (
         <div className="red">
-            <button className="btn btn-light">TRY IT NOW</button>
-            <h1>Superior sound</h1>
-            <h3>Experience live versions of your favourite songs</h3>
-            <button className="btn btn-light">SEE DEMO</button>
+            <div className="row">
+                <div className="col">
+                    <h1>Superior sound</h1>
+                    <h3>Experience live versions of your favourite songs</h3>
+                    <button className="btn btn-light">SEE DEMO</button>
+                </div>
+                <div className="col">
+                    <button className="btn btn-light">TRY IT NOW</button>
+                    <br/>
+                    <img src="./speaker1.png" className="w-25"/>
+                    <img src="./speaker2.png" className="w-25"/>
+                </div>
+            </div>
         </div>
   )
 }
